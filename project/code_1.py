331fbec3838e9530d7f9401e81094b2a1f80d8c5
HarshalVermaa <harshal.verma@hitoritech.com>
2024-12-11 23:39:35 +0530

    Add project folder

---
+++ project/code_1.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:26ec7bee825d98cf5515616d56b0d16f7c9608785af8255b9fce69198163213c
+size 3781
---
+++ project/code_1.spec
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0501a44547a89cf9f9acf8efe29be74b64aec0a134b40499eaf59fdfcef2cc80
+size 718
